dacc22138152a4d982996b7175d11832d84bb84a
Jonas Karg <jonas.karg@hotmail.com>
2020-04-26 21:58:41 +0200

    Device config stuff

--- keyboards/zedekiel/config.h
+++ keyboards/zedekiel/config.h
@@ -4,10 +4,10 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6066
+#define VENDOR_ID       0x03EB
+#define PRODUCT_ID      0x2FF4
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Jonas Karg
+#define MANUFACTURER    UAC
 #define PRODUCT         Zedekiel
 #define DESCRIPTION     Zedekiel Keyboard
 
@@ -16,18 +16,17 @@
 #define MATRIX_COLS 14
 
 // ROWS: Top to bottom, COLS: Left to right
-
-#define MATRIX_ROW_PINS { F7, F6, F5, F4, D5}
-#define MATRIX_COL_PINS { F1, F0, E6, C7, C6, B0, D4, B1, B7, B5, B4, D7, D6, B3}
+#define MATRIX_ROW_PINS { F0, F1, F4, F5, F6 }
+#define MATRIX_COL_PINS { F7, E6, C7, C6, D7, D6, D5, D4, D3, D2, B7, B6, B5, B0 }
 #define UNUSED_PINS
 
 /* ### Debugging  Pins ### */
-// #define MISO_PIN  xxxxxxxx
-// #define MOSI_PIN  xxxxxxxx
-// #define SCLK_PIN  xxxxxxxx
-// #define SDA_PIN   xxxxxxxx
-// #define SCL_PIN   xxxxxxxx
-// #define ADC11_PIN xxxxxxxx
+#define MISO_PIN  B3
+#define MOSI_PIN  B2
+#define SCLK_PIN  B1
+#define SDA_PIN   D1
+#define SCL_PIN   D0
+#define ADC11_PIN B4
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
